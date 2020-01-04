5118db31e4222d804f2c4a75f0ceb07765abc8ad
Honza <h.cech@seznam.cz>
2020-01-04 20:25:19 +0100

    add mountain car

---
+++ neural_network/MountainCarGym.py
@@ -0,0 +1,26 @@
+import gym
+
+class MountainCarGym:
+
+    def __init__(self):
+        self.env = gym.make('MountainCar-v0')
+
+    def close(self):
+        self.env.close()
+
+    def play(self, network, maxRounds = 1000, logActions = False):
+        env = self.env
+        observation = env.reset()
+        maxPosition = -1
+
+        for round in range(maxRounds):
+            env.render()
+            #action = env.action_space.sample()
+            action = network.evalToGetMax(observation)
+            observation, reward, done, info = env.step(action)
+            maxPosition = maxPosition if maxPosition > observation[0] else observation[0]
+            if done:
+                break
+
+        return maxPosition
+        
